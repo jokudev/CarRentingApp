7016a02c4bd573162ed6cd03d65962ca88f72832
jokudev <git@joku.dev>
2024-05-15 15:18:46 +0200

    add routes and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SigninComponent } from './components/signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'signin', component: SigninComponent }
+];
